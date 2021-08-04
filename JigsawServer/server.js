cf1dc57447e8f3e1516db7fd4556862a5ce7a698
r4ksh1t0011 <rakshitawasthi17@gmail.com>
2021-08-04 18:36:34 +0530

    Updated the server

--- JigsawServer/server.js
+++ JigsawServer/server.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const crypto = require('crypto-js');
+const nodemailer = require('nodemailer');
 const saltRounds = 10;
+require('dotenv').config();
 var user = "";
+var email1 = "";
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
@@ -18,12 +22,21 @@ const JigsawSchema = mongoose.Schema({
     highScore:Number
 })
 const JigsawUsers = mongoose.model("jigsawUser",JigsawSchema);
+
+// Email validation using Regex
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  
+ 
+// Register handler
 app.post("/register",(req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
-    console.log(req.body);
-    
+    const valid = validateEmail(email);
+    if(valid){
     JigsawUsers.findOne({name:username},(err,data)=>{
         if(data===null){
             JigsawUsers.findOne({email:email},(err1,data1)=>{
@@ -53,11 +66,17 @@ app.post("/register",(req,res)=>{
             res.send("Username is already taken");
         }
     })
+}
+    else{
+        res.send("Invalid");
+    }
     
 })
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 })
+
+// Login Handler
 app.post("/login",(req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
@@ -88,7 +107,6 @@ app.get("/login",(req,res)=>{
     res.send(user);
 })
 app.post("/scoreboard",(req,res)=>{
-    console.log(req.body);
     var score = req.body.score;
     var scr;
     score = parseFloat(score);
@@ -117,6 +135,7 @@ app.post("/scoreboard",(req,res)=>{
     
 });
 
+// The scoreboard
 app.get("/scoreboard",(req,res)=>{
     let dataToSend = [];
     JigsawUsers.find().sort('-highScore').exec((err,data)=>{
@@ -136,4 +155,137 @@ app.get("/scoreboard",(req,res)=>{
     })
     
 })
+var hash="hash";
+// Reset login details by using email
+app.get("/reset",(req,res)=>{
+    let num = Math.floor(100*Math.random());
+    num = String(num);
+    let em = String(req.body.email);
+    email1 = req.body.email;
+    em+=num;
+    hash = crypto.MD5(em);
+    url = `https://jigsaw-real.herokuapp.com/reset/${hash}`;
+    hash = `${hash}`
+    
+    // let email = data.email;
+    const transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:process.env.EMAIL,
+            pass:process.env.PASSWORD
+        }
+
+    });
+    const mailOptions = {
+        from:process.env.EMAIL,
+        to:email1,
+        subject:'Password/Username Reset',
+        html:`<div><h2>The password reset link is &nbsp.;</h2><a href=${url}>${url}</a><h2>Ignore this email if you didn't request for changing your password fot the Real World Jigsaw game</h2></div>`
+    };
+    transporter.sendMail(mailOptions,(error,info)=>{
+        if(error){
+            res.send("Error");
+        }
+        res.send("Done");
+    })
+    app.get(`/reset/${hash}`,(req,res)=>{
+        res.sendFile(__dirname+"/reset.html")
+    })
+})
+app.post("/reset",(req,res)=>{
+    let username = req.body.username;
+    let password = req.body.password;
+    if(username.length==0&& password.length==0){
+        res.send("No");
+    }
+    else if(username.length==0 && password.length!=0){
+        bcrypt.hash(password,saltRounds,(error,hash)=>{
+            JigsawUsers.findOne({email:email1},(err,data)=>{
+                if(err){
+                    res.send("No");
+                }
+                else{
+                    if(data==null){
+                        res.send("Invalid");
+                    }
+                    else{
+                        JigsawUsers.updateOne({email:email1},{
+                            $set:{
+                                password:hash
+                            }
+                        },(error1,data1)=>{
+                            if(error1){
+                                res.send("No");
+                            }
+                            else{
+                                res.send("Updated");
+                            }
+                        })
+                    }
+                }
+            })
+        })
+        
+    }
+    else if(username.length!=0 && password.length==0){
+        JigsawUsers.findOne({email:email1},(err2,data2)=>{
+            if(err2){
+                res.send("No");
+            }
+            else{
+                if(data2==null){
+                    res.send("Invalid");
+                }
+                else{
+                    JigsawUsers.updateOne({email:email1},{
+                        $set:{
+                            name:username
+                        }
+                    },(err3,data3)=>{
+                        if(err3){
+                            res.send("No");
+                        }
+                        else{
+                            res.send("Updated");
+                        }
+                    })
+                }
+            }
+        })
+    }
+    else if(username.length!=0 && password.length!=0){
+        bcrypt.hash(password,saltRounds,(err4,hash)=>{
+            JigsawUsers.findOne({email:email1},(err5,data5)=>{
+                if(err5){
+                    res.send("No");
+                }
+                else{
+                    if(data5==null){
+                        res.send("Invalid");
+                    }
+                    else{
+                        JigsawUsers.updateOne({email:email1},{
+                            $set:{
+                                name:username,
+                                password:hash
+                            }
+                        },(err6,data6)=>{
+                            if(err6){
+                                res.send("No");
+                            }
+                            else{
+                                res.send("Updated");
+                            }
+                        })
+                    }
+                }
+            })
+        })
+    }
+    // res.redirect("/reset/redirect");
+})
+app.get("/reset/redirect",(req,res)=>{
+    res.sendFile(__dirname+"/changed.html");
+})
+
 app.listen(process.env.PORT||3500);
